1d57cadef21b01b78bc7b54323d1dff04f3358e6
Michael Chan <michaelcychan@gmail.com>
2022-05-10 15:53:17 +0100

    change if clause

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,12 +1,11 @@
 def fizzbuzz(num)
-  if num % 5 == 0
-    return 'buzz'
-  elsif num % 3 == 0
-    return 'fizz'
+  if num < 1
+    return num
   elsif num % 3 == 0 && num % 5 == 0
     return 'fizzbuzz'
-  else
-    return num
-   
+  elsif num % 3 == 0
+    return 'fizz'
+  elsif num % 5 == 0
+    return 'buzz'  
   end
 end
